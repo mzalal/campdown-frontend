d29574fcd80806507f0fd9a17ac2a394ccfe2229
M. Zalal <mzalal@proton.me>
2025-07-17 19:52:06 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>Hello, World!</h1>
+            <p>Lorem ipsum dolor sit.</p>
+        </>
+    );
+}
+
+export default App;
