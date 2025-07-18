7bd556d153b9907504e1e3036ab3ef24ba5b5318
M. Zalal <mzalal@proton.me>
2025-07-18 17:02:39 +0100

    Add `main` element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import NavBar from "./components/NavBar/NavBar.jsx";
+import FilterBar from "./components/FilterBar/FilterBar.jsx";
 import "./App.css";
 
 function App() {
     return (
         <>
             <NavBar />
+            <main>
+                <FilterBar />
+            </main>
         </>
     );
 }
