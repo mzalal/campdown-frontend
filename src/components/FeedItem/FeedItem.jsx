a88f364e6d7c98cb03e061167f0a2efc11ea1d1e
M. Zalal <mzalal@proton.me>
2025-08-13 23:52:29 +0100

    Replace hard-coded elements with components

--- src/components/FeedItem/FeedItem.jsx
+++ src/components/FeedItem/FeedItem.jsx
@@ -1,32 +1,14 @@
-import { useState } from "react";
-import Icon from "../Icon/Icon.jsx";
+import EventHeading from "../EventHeading/EventHeading.jsx";
+import EventActions from "../EventActions/EventActions.jsx";
+import UpdatesCard from "../UpdatesCard/UpdatesCard.jsx";
 import classes from "./FeedItem.module.css";
 
 function FeedItem() {
-    const [isStoked, setIsStoked] = useState(false);
-
-    function handleStokeClick() {
-        setIsStoked((prev) => !prev);
-    }
-
     return (
         <div className={classes.feedItem}>
-            <div className={classes.heading}>
-                <h2>Lorem ipsum dolor sit amet</h2>
-                <div className={classes.countdown}>00:00:00:00</div>
-            </div>
+            <EventHeading />
             <div className={classes.body}>
-                <div className={classes.updatesBox}>
-                    <div className={classes.timeTag}>
-                        Latest update — Today, 00:00
-                    </div>
-                    <p className={classes.updateMessage}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                </div>
+                <UpdatesCard />
                 <div className={classes.chatBox}>
                     <p className={classes.chatMessage}>
                         <span className={classes.username}>username:</span>
@@ -41,21 +23,8 @@ function FeedItem() {
                         Lorem ipsum dolor sit amet
                     </p>
                 </div>
-                <div className={classes.actions}>
-                    <button className={classes.otherOptions}>
-                        <Icon name="ellipsis-horizontal" />
-                    </button>
-                    <div className={classes.viewers}>
-                        <Icon name="users" style="solid" />
-                        <div>000</div>
-                    </div>
-                    <button
-                        onClick={handleStokeClick}
-                        className={isStoked && classes.stoked}
-                    >
-                        <Icon name="fire" style={isStoked && "solid"} />
-                        <div>000</div>
-                    </button>
+                <div className={classes.eventActionsContainer}>
+                    <EventActions />
                 </div>
             </div>
         </div>
